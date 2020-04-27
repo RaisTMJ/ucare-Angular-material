fd9228755ae482c5be463e5ee3ba337014fbbc7e
Mohamad Rais <rais95mohamad@gmail.com>
2020-04-27 09:48:44 +0800

    update app module import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ImGridComponent} from './im-grid/im-grid.component';
-import {MatGridListModule, MatIconModule} from '@angular/material';
 import {FlexModule} from '@angular/flex-layout';
 import {UcWidgetModule} from 'ngx-uploadcare-widget';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
